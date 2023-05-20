fed1e192ef3ddbfe44eb2aa4e8fa42e02134314d
hmcclew <122323895+hmcclew@users.noreply.github.com>
2023-05-20 18:45:29 -0400

    implemented question collection class

--- src/main/java/cs3500/pa01/contentCollection/QuestionCollection.java
+++ src/main/java/cs3500/pa01/contentCollection/QuestionCollection.java
@@ -1,4 +1,47 @@
 package cs3500.pa01.contentCollection;
 
-public class QuestionCollection {
+import cs3500.pa01.contentCollection.questions.AbstractQuestion;
+import java.util.ArrayList;
+
+/**
+ * Represents a collection of questions
+ */
+public class QuestionCollection implements ContentCollection {
+
+  private ArrayList<AbstractQuestion> questionCollection = new ArrayList<>();
+
+  /**
+   * Adds a question to this instance's questionCollection
+   *
+   * @param q the question to be added
+   */
+  public void add(AbstractQuestion q) {
+    questionCollection.add(q);
+  }
+
+  /**
+   * Gets the question in this instance's questionCollection at the given index
+   *
+   * @param index the index of the question to be returned
+   * @return the question at the given index of questionCollection
+   */
+  @Override
+  public AbstractQuestion get(int index) {
+    return questionCollection.get(index);
+  }
+
+  /**
+   * Converts the content of this QuestionCollection to a single string
+   *
+   * @return the new string of this QuestionCollection's content
+   */
+  @Override
+  public String toString() {
+    StringBuilder s = new StringBuilder();
+    for (AbstractQuestion q : this.questionCollection) {
+        s.append(q);
+      }
+    return s.toString();
+  }
 }
+
