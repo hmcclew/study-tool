06fe400e4ee0c70d8d73a867c617a9b730a56379
hmcclew <122323895+hmcclew@users.noreply.github.com>
2023-05-22 10:29:08 -0400

    adding tests

--- src/test/java/cs3500/pa01/reader/InputReaderTest.java
+++ src/test/java/cs3500/pa01/reader/InputReaderTest.java
@@ -1,22 +1,19 @@
-package cs3500.pa01.sessionwriter;
+package cs3500.pa01.reader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import cs3500.pa01.contentcollection.QuestionCollection;
 import cs3500.pa01.contentcollection.question.EasyQuestion;
 import cs3500.pa01.contentcollection.question.HardQuestion;
-import cs3500.pa01.updater.SessionStatisticsUpdater;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-class InSessionWriterTest {
+class InputReaderTest {
+
+  InputReader ip;
 
   QuestionCollection qc;
-
-  InSessionWriter isw;
 
   @BeforeEach
   public void setup() {
@@ -27,14 +24,27 @@ class InSessionWriterTest {
     ArrayList<HardQuestion>
         hardQuestions = new ArrayList<>(Arrays.asList(hardQuestion1, hardQuestion2));
     ArrayList<EasyQuestion> easyQuestions = new ArrayList<>(Arrays.asList(easyQuestion1, easyQuestion2));
-    qc = new QuestionCollection(hardQuestions, easyQuestions, 4);
-    isw = new InSessionWriter(qc);
+    qc = new QuestionCollection(hardQuestions, easyQuestions, 10);
+    ip = new InputReader(qc);
   }
-  @Test
-  public void testWrite() {
-    //isw.write();
+
+  public void testReader() {
+    assertEquals(2, qc.numEasyQuestions());
+    assertEquals(2, qc.numHardQuestions());
+
+    ip.setCurrentQuestion(qc.getQuestionCollection().get(0));
+    ip.read(UserInput.ONE.getValue());
+
+    assertEquals(3, qc.numEasyQuestions());
+    assertEquals(1, qc.numHardQuestions());
+
+    ip.setCurrentQuestion(qc.getQuestionCollection().get(3));
+    ip.read(UserInput.TWO.getValue());
+
+    assertEquals(2, qc.numEasyQuestions());
+    assertEquals(2, qc.numHardQuestions());
+
+    assertEquals(qc, ip.getQuestionCollection());
   }
+
 }
-
-
-
